12f659e8ef61951c0745277f0df37d0e5ac3fed4
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-08-20 21:05:01 +0200

    Add testing library

--- src/utils/HashSet.ts
+++ src/utils/HashSet.ts
@@ -1,11 +1,4 @@
 import { Hashable } from "./interfaces/Hashable";
 
 export class HashSet<T extends Hashable<T>> {
-  equals(other: T): boolean {
-    return true;
-  }
-
-  hash(): number {
-    return 5;
-  }
 }
