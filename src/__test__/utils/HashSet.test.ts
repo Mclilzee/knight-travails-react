45fb0c2542f1bc8904354f07ccb3c743f3ac825f
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-08-22 23:29:04 +0200

    Start testing HashSet

--- src/__test__/utils/HashSet.test.ts
+++ src/__test__/utils/HashSet.test.ts
@@ -1,6 +1,25 @@
-import { describe, test } from "vitest";
+import { describe, test, expect } from "vitest";
+import { Hashable } from "../../main/utils/interfaces/Hashable";
+import { HashSet } from "../../main/utils/HashSet";
 
-describe("This is a test", () => {
-  test("this is the test", () => {
+describe("Add elements to the set", () => {
+  test("Adds element to the set", () => {
+    const set: HashSet<MockHashable> = new HashSet();
+    const mockObject = new MockHashable();
+
+    expect(set.length()).toBe(0);
+    set.add(mockObject)
+    expect(set.length()).toBe(1);
   })
 })
+
+class MockHashable implements Hashable<MockHashable> {
+  hash(): number {
+    return 0;
+  }
+
+  equals(other: MockHashable) {
+    other.hash();
+    return true;
+  }
+}
--- src/main/utils/HashSet.ts
+++ src/main/utils/HashSet.ts
@@ -1,4 +1,25 @@
 import { Hashable } from "./interfaces/Hashable";
 
 export class HashSet<T extends Hashable<T>> {
+  private array: T[][];
+  private size: number;
+
+  constructor() {
+    this.array = [];
+    console.log(this.array);
+    this.size = 0;
+  }
+
+  add(element: T): void {
+    if (this.array[0] === undefined) {
+      this.array[0] = [];
+    }
+
+    this.array[0].push(element);
+    this.size++;
+  }
+
+  length(): number {
+    return this.size;
+  }
 }
