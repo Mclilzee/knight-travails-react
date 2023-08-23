15ad1f39404496604cacdbfd1cc62d1a3adec614
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-08-23 05:54:16 +0200

    Complete hashset

--- src/__test__/utils/HashSet.test.ts
+++ src/__test__/utils/HashSet.test.ts
@@ -5,23 +5,124 @@ import { HashSet } from "../../main/utils/HashSet";
 describe("Add elements to the set", () => {
   test("Adds element to the set", () => {
     const set: HashSet<MockHashable> = new HashSet();
-    const mockObject = new MockHashable(0, true);
+    const mockObject = new MockHashable(0);
 
     expect(set.length()).toBe(0);
+
     set.add(mockObject)
     expect(set.length()).toBe(1);
   });
 
   test("Add more than one element returns the correct length", () => {
     const set: HashSet<MockHashable> = new HashSet();
-    const mockObject = new MockHashable(0, false);
-    const mockObject2 = new MockHashable(1, false);
+    const mockObject = new MockHashable(0);
+    const mockObject2 = new MockHashable(1);
 
     expect(set.length()).toBe(0);
+
     set.add(mockObject)
     expect(set.length()).toBe(1);
+
     set.add(mockObject2)
     expect(set.length()).toBe(2);
+  });
+
+  test("Does not add if element already exist", () => {
+    const set: HashSet<MockHashable> = new HashSet();
+    const mockObject = new MockHashable(0, true);
+    const mockObject2 = new MockHashable(0, true);
+
+    expect(set.length()).toBe(0);
+
+    set.add(mockObject)
+    expect(set.length()).toBe(1);
+
+    set.add(mockObject2)
+    expect(set.length()).toBe(1);
+  });
+
+  test("Does add if elements has same hash but are not equal", () => {
+    const set: HashSet<MockHashable> = new HashSet();
+    const mockObject = new MockHashable(0);
+    const mockObject2 = new MockHashable(0);
+
+    expect(set.length()).toBe(0);
+
+    set.add(mockObject)
+    expect(set.length()).toBe(1);
+
+    set.add(mockObject2)
+    expect(set.length()).toBe(2);
+  });
+});
+
+describe("Check if set contains an element", () => {
+  test("Set does not contain element if it's empty", () => {
+    const set: HashSet<MockHashable> = new HashSet();
+    const mockObject = new MockHashable(0);
+
+    expect(set.contains(mockObject)).toBe(false);
+  });
+
+  test("Set does contain element that is added", () => {
+    const set: HashSet<MockHashable> = new HashSet();
+    const mockObject = new MockHashable(0);
+
+    set.add(mockObject);
+    expect(set.contains(mockObject)).toBe(true);
+  });
+
+  test("Set does not contain element that has same hash value but are not equal", () => {
+    const set: HashSet<MockHashable> = new HashSet();
+    const mockObject = new MockHashable(0);
+    const mockObject2 = new MockHashable(0);
+
+    set.add(mockObject);
+    expect(set.contains(mockObject2)).toBe(false);
+  });
+});
+
+describe("Set removes the correct elements", () => {
+  test("Removes does not error on empty set", () => {
+    const set: HashSet<MockHashable> = new HashSet();
+    const mockObject = new MockHashable(0);
+
+    expect(set.length()).toBe(0);
+
+    set.remove(mockObject);
+    expect(set.contains(mockObject)).toBe(false);
+    expect(set.length()).toBe(0);
+  });
+
+  test("Removes the correct element", () => {
+    const set: HashSet<MockHashable> = new HashSet();
+    const mockObject = new MockHashable(0);
+
+    expect(set.length()).toBe(0);
+
+    set.add(mockObject);
+    expect(set.length()).toBe(1);
+    expect(set.contains(mockObject)).toBe(true);
+
+    set.remove(mockObject);
+    expect(set.length()).toBe(0);
+    expect(set.contains(mockObject)).toBe(false);
+  })
+
+  test("Does not remove element that has same hash if not equal", () => {
+    const set: HashSet<MockHashable> = new HashSet();
+    const mockObject = new MockHashable(0);
+    const mockObject2 = new MockHashable(0);
+
+    expect(set.length()).toBe(0);
+
+    set.add(mockObject);
+    expect(set.length()).toBe(1);
+    expect(set.contains(mockObject)).toBe(true);
+
+    set.remove(mockObject2);
+    expect(set.length()).toBe(1);
+    expect(set.contains(mockObject)).toBe(true);
   })
 });
 
@@ -29,9 +130,14 @@ class MockHashable implements Hashable<MockHashable> {
   hashNumber: number;
   equality: boolean;
 
-  constructor(hashNumber: number, equality: boolean) {
+  constructor(hashNumber: number, equality?: boolean) {
+    if (equality != null) {
+      this.equality = equality;
+    } else {
+      this.equality = false;
+    }
+
     this.hashNumber = hashNumber;
-    this.equality = equality;
   }
 
   hash(): number {
@@ -39,7 +145,6 @@ class MockHashable implements Hashable<MockHashable> {
   }
 
   equals(other: MockHashable) {
-    other.hash();
-    return this.equality;
+    return this.equality || (this === other);
   }
 }
--- src/main/utils/HashSet.ts
+++ src/main/utils/HashSet.ts
@@ -14,10 +14,50 @@ export class HashSet<T extends Hashable<T>> {
     if (!this.map.has(key)) {
       this.map.set(key, []);
     }
-    const array = this.map.get(key);
+    const array = this.map.get(key) as T[];
 
-    array?.push(element);
+    for (const eachElement of array) {
+      if (eachElement.equals(element)) {
+        return;
+      }
+    }
+
+    array.push(element);
     this.size++;
+  }
+
+  contains(element: T): boolean {
+    const key = element.hash();
+    const array = this.map.get(key);
+    if (array == undefined) {
+      return false;
+    }
+
+    for (const eachElement of array) {
+      if (eachElement.equals(element)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  remove(element: T): void {
+    const key = element.hash();
+    if (!this.map.has(key)) {
+      return;
+    }
+
+    const array = this.map.get(key) as T[];
+    for (let i = 0; i < array.length; i++) {
+      const arrayElement = array[i];
+      if (arrayElement.equals(element)) {
+        const newArray = array.slice(0, i);
+        newArray.concat(array.slice(i + 1));
+        this.map.set(key, newArray);
+        this.size--;
+      }
+    }
   }
 
   length(): number {
