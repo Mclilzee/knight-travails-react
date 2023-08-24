ceea1723459265b7c75810b3b451791af3f1c59f
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-08-24 22:57:46 +0200

    Update Knight class to only used allowed numbers ranged

--- src/main/utils/chess/Knight.ts
+++ src/main/utils/chess/Knight.ts
@@ -1,10 +1,12 @@
 export default class Knight {
   position: Cord;
 
-  constructor(row: number, column: number) {
-    this.position = [row, column]
+  constructor(row: AllowedPositions, column: AllowedPositions) {
+    this.position = [row, column];
   }
 }
+
+type AllowedPositions = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
 
 export type Cord = [number, number];
 
