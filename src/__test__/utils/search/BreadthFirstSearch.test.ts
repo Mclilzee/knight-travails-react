88bbc401b86d83084ffbcbc71ed949892fc20fc7
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-08-25 13:58:19 +0200

    Add breadth search structure

--- src/__test__/utils/search/BreadthFirstSearch.test.ts
+++ src/__test__/utils/search/BreadthFirstSearch.test.ts
@@ -5,12 +5,23 @@ import BreadthFirstSearch from "../../../main/utils/search/BreadthFirstSearch";
 
 describe("Returns correct path", () => {
   const search: PathFinder<ChessSquare> = new BreadthFirstSearch();
-  test("Returns same node if it's the destination", () => {
+
+  test("Returns same start if it's the goal", () => {
     const start = new ChessSquare(0, 0);
     const goal = new ChessSquare(0, 0);
 
     const actual: ChessSquare[] = search.findPath(start, goal);
     expect(actual.length).toBe(1);
     expect(actual[0]).toBe(start);
+  });
+
+  test("Return correrct squares in order", () => {
+    const start = new ChessSquare(0, 0);
+    const goal = new ChessSquare(1, 2);
+
+    const actual: ChessSquare[] = search.findPath(start, goal);
+    expect(actual.length).toBe(2);
+    expect(actual[0]).toBe(start);
+    expect(actual[1]).toBe(goal);
   });
 });
--- src/main/utils/search/BreadthFirstSearch.ts
+++ src/main/utils/search/BreadthFirstSearch.ts
@@ -3,8 +3,23 @@ import { PathFinder } from "../interfaces";
 import Node from "./Node";
 
 export default class BreadthFirstSearch implements PathFinder<ChessSquare> {
+  start: Node;
+  goal: Node;
+
+  constructor() {
+    const firstSquare = new ChessSquare(0, 0);
+    this.start = new Node(firstSquare);
+    this.goal = new Node(firstSquare);
+  }
 
   findPath(start: ChessSquare, goal: ChessSquare): ChessSquare[] {
-    return [start];
+    this.start = new Node(start);
+    this.goal = new Node(goal);
+
+    if (this.start.equals(this.goal)) {
+      return [this.start.chessSquare];
+    } else {
+      return [this.start.chessSquare, this.goal.chessSquare];
+    }
   }
 }
