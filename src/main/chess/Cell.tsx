b7c631ac9c558b0e118ffb81ee841faedc101052
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-08-26 23:33:42 +0200

    Update cells to dynamicly display properties

--- src/main/chess/Cell.tsx
+++ src/main/chess/Cell.tsx
@@ -1,25 +1,13 @@
-import ChessSquare from "../utils/ChessSquare";
 import { ChessBoardCell } from "./interfaces";
 
 export default function Cell({ square, step, selected, knight }: ChessBoardCell) {
 
   return (
     <div className={
-      `cell 
-      ${getCellColor(square)}
-      ${selected ? "selected" : ""}
-      ${knight ? "knight" : ""}
-      `} >
-      {step > 0 ? step : ""}
+      `cell ${(square.x + square.y) % 2 === 0 ? "white" : "black"}`} >
+      {knight && <img src="../assets/knight.svg" />}
+      {!knight && selected && <div></div>}
+      {!knight && !selected && step > 0 && step}
     </div >
   )
 }
-
-function getCellColor(square: ChessSquare) {
-  if ((square.x + square.y) % 2 === 0) {
-    return "white"
-  } else {
-    return "black"
-  }
-}
-
