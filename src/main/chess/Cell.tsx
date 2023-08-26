39a425504f41b023fb8259ab1259c3ecce747690
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-08-26 23:11:07 +0200

    Update cells to reflect their status

--- src/main/chess/Cell.tsx
+++ src/main/chess/Cell.tsx
@@ -1,8 +1,20 @@
+import ChessSquare from "../utils/ChessSquare";
 import { ChessBoardCell } from "./interfaces";
 
-export default function Cell(props: ChessBoardCell) {
+export default function Cell({ square, step, selected }: ChessBoardCell) {
+
   return (
-    <div className="cell"></div>
+    <div className={`cell ${getCellColor(square)} ${selected ? "selected" : ""}`} >
+      {step > 0 ? step : ""}
+    </div >
   )
+}
+
+function getCellColor(square: ChessSquare) {
+  if ((square.x + square.y) % 2 === 0) {
+    return "white"
+  } else {
+    return "black"
+  }
 }
 
