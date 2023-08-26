ecea8c2c8d7b8bf7253cebab51ca90fabccea240
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-08-26 23:47:46 +0200

    Knight appears on the background for each cell that contains it

--- src/main/chess/Cell.tsx
+++ src/main/chess/Cell.tsx
@@ -6,7 +6,7 @@ export default function Cell({ square, step, selected, knight }: ChessBoardCell)
   return (
     <div className={
       `cell ${(square.x + square.y) % 2 === 0 ? "white" : "black"}`} >
-      {knight && <img src="../assets/knight.svg" />}
+      {knight && <img className="knight" src={knightSvg} />}
       {!knight && selected && <div></div>}
       {!knight && !selected && step > 0 && step}
     </div >
