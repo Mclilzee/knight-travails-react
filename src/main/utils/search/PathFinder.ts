b18cf526a2babafebef3c300f70879ab76b64b54
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-08-27 10:33:14 +0200

    Update Pathfinder to use new tuple structure

--- src/main/utils/search/PathFinder.ts
+++ src/main/utils/search/PathFinder.ts
@@ -3,31 +3,30 @@ import Node from "./Node";
 
 export default class PathFinder {
 
-  findShortestPath(start: [number, number], goal: [number, number]): ChessSquare[] {
+  findShortestPath(start: [number, number], goal: [number, number]): [number, number][] {
     const visited: TupleSet = new TupleSet();
-    const startNode = new Node(start);
-    const goalNode = new Node(goal);
-    const queue: Node[] = [startNode];
+    const queue: Node[] = [new Node(start)];
 
     while (queue.length > 0) {
       const node = queue[0];
+      const location = node.location;
       queue.splice(0, 1);
-      visited.add(node);
+      visited.add(location);
 
-      if (node.equals(goalNode)) {
+      if (location[0] === goal[0] && location[1] === goal[1]) {
         return this.buildSquaresArray(node);
       }
 
       node.getMoves()
-        .filter((move) => !visited.contains(move))
+        .filter((move) => !visited.has(move.location))
         .forEach((move) => queue.push(move));
     }
 
     throw Error("Knight should always find the path");
   }
 
-  visitAllSquares(start: ChessSquare): ChessSquare[] {
-    let visited: TupleSet<Node> = new TupleSet();
+  visitAllSquares(start: [number, number]): [number, number][] {
+    let visited: TupleSet = new TupleSet();
     const startNode = new Node(start);
     const result = this.recursivelyVisitNodes(startNode, visited);
     if (result !== null) {
@@ -37,13 +36,13 @@ export default class PathFinder {
     throw Error("Could not find solution, which is impossible");
   }
 
-  private recursivelyVisitNodes(start: Node, visited: TupleSet<Node>): Node | null {
+  private recursivelyVisitNodes(start: Node, visited: TupleSet): Node | null {
     if (start.length === 64) {
       return start;
     }
 
-    visited.add(start);
-    const moves = start.getMoves().filter((move) => !visited.contains(move));
+    visited.add(start.location);
+    const moves = start.getMoves().filter((move) => !visited.has(move.location));
     for (const move of moves) {
       const result = this.recursivelyVisitNodes(move, visited)
       if (result !== null) {
@@ -51,16 +50,16 @@ export default class PathFinder {
       }
     }
 
-    visited.remove(start);
+    visited.remove(start.location);
     return null;
   }
 
-  private buildSquaresArray(node: Node): ChessSquare[] {
+  private buildSquaresArray(node: Node): [number, number][] {
     let parent = node.parent;
-    const squares: ChessSquare[] = [node.chessSquare];
+    const squares = [node.location];
 
     while (parent !== null) {
-      squares.push(parent.chessSquare);
+      squares.push(parent.location);
       parent = parent.parent;
     }
 
